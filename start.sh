82e3b2be0067933edeb583fdc1c30ea719313636
Nestor Ayuso <nestorayuso@gmail.com>
2016-02-24 11:21:16 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ sleep 0.1
 
 # Test the connection, wait if needed.
 while [[ $(ping -c1 google.com 2>&1 | grep " 0% packet loss") == "" ]]; do
-  echo "[TTN Gateway]: Waiting for internet connection..."
+  echo "[LoRa Gateway]: Waiting for internet connection..."
   sleep 30
   done
 
